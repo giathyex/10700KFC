90a94be12d40af4697d0751dca585144ef57d521
LittleLegend-Pengiun <hoanganhleboy@gmail.com>
2021-11-20 19:36:16 +0700

    rearrange code

--- kitchen/assets/scripts/kitchen.js
+++ kitchen/assets/scripts/kitchen.js
@@ -25,6 +25,49 @@ document.getElementById("pass-enter").addEventListener("input", function() {
     }
 });
 
+// Number comma formatting
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+function createFormOrder(doc) {
+    var div = document.createElement("div");
+    div.className = "form-titles";
+    var tableNo = document.createElement("span");
+    tableNo.className = "flex-item-first";
+    var foodList = document.createElement("span");
+    foodList.className = "flex-item-secnd";
+    var totalPrice = document.createElement("span");
+    totalPrice.className = "flex-item-third";
+    var button = document.createElement("button");
+    button.className = "flex-item-four";
+    button.innerHTML = "Hoàn thành";
+
+    div.classList.add('form-info');
+
+    div.setAttribute("data-id", doc.id);
+    tableNo.textContent = doc.data().table;
+    totalPrice.textContent = numberWithCommas(doc.data().total);
+    foodList.innerHTML = "";
+    doc.data().food_name.forEach(food => {
+        foodList.innerHTML = foodList.innerHTML + food + '<br/>';
+    });
+    // icon.classList.add("far", "fa-trash-alt");
+
+    div.appendChild(tableNo);
+    div.appendChild(foodList);
+    div.appendChild(totalPrice);
+    div.appendChild(button);
+
+    formData.appendChild(div);
+
+    button.addEventListener('click', () => {
+        var ref = db.collection("orders").doc(doc.id);
+        ref.delete();
+    });
+}
+
+
 // Sign-in button
 const signinn = document.getElementById("sign-in");
 signinn.addEventListener('click', signinnf);
@@ -42,49 +85,7 @@ function signinnf() {
                     document.getElementById("signinpage").style.display = "none";
                     document.getElementById("mainpage").style.display = "";
 
-                    // Number comma formatting
-                    function numberWithCommas(x) {
-                        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-                    }
-
                     const formData = document.querySelector(".form-data");
-
-                    function createFormOrder(doc) {
-                        var div = document.createElement("div");
-                        div.className = "form-titles";
-                        var tableNo = document.createElement("span");
-                        tableNo.className = "flex-item-first";
-                        var foodList = document.createElement("span");
-                        foodList.className = "flex-item-secnd";
-                        var totalPrice = document.createElement("span");
-                        totalPrice.className = "flex-item-third";
-                        var button = document.createElement("button");
-                        button.className = "flex-item-four";
-                        button.innerHTML = "Hoàn thành";
-
-                        div.classList.add('form-info');
-
-                        div.setAttribute("data-id", doc.id);
-                        tableNo.textContent = doc.data().table;
-                        totalPrice.textContent = numberWithCommas(doc.data().total);
-                        foodList.innerHTML = "";
-                        doc.data().food_name.forEach(food => {
-                            foodList.innerHTML = foodList.innerHTML + food + '<br/>';
-                        });
-                        // icon.classList.add("far", "fa-trash-alt");
-
-                        div.appendChild(tableNo);
-                        div.appendChild(foodList);
-                        div.appendChild(totalPrice);
-                        div.appendChild(button);
-
-                        formData.appendChild(div);
-
-                        button.addEventListener('click', () => {
-                            var ref = db.collection("orders").doc(doc.id);
-                            ref.delete();
-                        });
-                    }
 
                     // Show orders real-time
                     db.collection("orders").onSnapshot((snap) => {
