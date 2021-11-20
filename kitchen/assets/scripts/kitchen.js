63343948ac7c8802e7edde5a5ce1e9d8ca0e2aec
Trần Đình Gia Hải <60392917+giathyex@users.noreply.github.com>
2021-11-20 18:58:16 +0700

    password

--- kitchen/assets/scripts/kitchen.js
+++ kitchen/assets/scripts/kitchen.js
@@ -13,69 +13,6 @@ db.settings({
     timestampsInSnapshots: true
 });
 
-// Number comma formatting
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-}
-
-const formData = document.querySelector(".form-data");
-
-function createFormOrder(doc) {
-    var div = document.createElement("div");
-    div.className = "form-titles";
-    var tableNo = document.createElement("span");
-    tableNo.className = "flex-item-first";
-    var foodList = document.createElement("span");
-    foodList.className = "flex-item-secnd";
-    var totalPrice = document.createElement("span");
-    totalPrice.className = "flex-item-third";
-    var button = document.createElement("button");
-    button.className = "flex-item-four";
-    button.innerHTML = "Hoàn thành";
-
-    div.classList.add('form-info');
-
-    div.setAttribute("data-id", doc.id);
-    tableNo.textContent = doc.data().table;
-    totalPrice.textContent = numberWithCommas(doc.data().total);
-    foodList.innerHTML = "";
-    doc.data().food_name.forEach(food => {
-        foodList.innerHTML = foodList.innerHTML + food + '<br/>';
-    });
-    // icon.classList.add("far", "fa-trash-alt");
-
-    div.appendChild(tableNo);
-    div.appendChild(foodList);
-    div.appendChild(totalPrice);
-    div.appendChild(button);
-
-    formData.appendChild(div);
-
-    button.addEventListener('click', () => {
-        var ref = db.collection("orders").doc(doc.id);
-        ref.delete();
-    });
-}
-
-// Show orders real-time
-db.collection("orders").onSnapshot((snap) => {
-    snap.docChanges().forEach((doc) => {
-        if (doc.type === "added") {
-            createFormOrder(doc.doc);
-        } else if (doc.type === "removed") {
-            var div = document.querySelector(`[data-id=${doc.doc.id}]`);
-            try {
-                formData.removeChild(div);
-            }
-            catch {
-                window.location.reload();
-            }
-            
-        }
-    })
-})
-
-
 // Enable enter table box after some stuff is added to cart
 document.getElementById("pass-enter").addEventListener("input", function() {
     var passs = document.getElementById("pass-enter").value;
@@ -93,5 +30,88 @@ const signinn = document.getElementById("sign-in");
 signinn.addEventListener('click', signinnf);
 
 function signinnf() {
-    console.log(1);
+    var namew = document.getElementById("name-enter").value;
+    var passw = document.getElementById("pass-enter").value;
+    var namei = namew + "t";
+    try {
+        db.collection(namew).doc(passw).get().then(doc => {
+            //Valid code
+            if (doc.exists) {
+                // Valid password
+                if (doc.get(namei) != null) {
+                    document.getElementById("signinpage").style.display = "none";
+                    document.getElementById("mainpage").style.display = "";
+
+                    // Number comma formatting
+                    function numberWithCommas(x) {
+                        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+                    }
+
+                    const formData = document.querySelector(".form-data");
+
+                    function createFormOrder(doc) {
+                        var div = document.createElement("div");
+                        div.className = "form-titles";
+                        var tableNo = document.createElement("span");
+                        tableNo.className = "flex-item-first";
+                        var foodList = document.createElement("span");
+                        foodList.className = "flex-item-secnd";
+                        var totalPrice = document.createElement("span");
+                        totalPrice.className = "flex-item-third";
+                        var button = document.createElement("button");
+                        button.className = "flex-item-four";
+                        button.innerHTML = "Hoàn thành";
+
+                        div.classList.add('form-info');
+
+                        div.setAttribute("data-id", doc.id);
+                        tableNo.textContent = doc.data().table;
+                        totalPrice.textContent = numberWithCommas(doc.data().total);
+                        foodList.innerHTML = "";
+                        doc.data().food_name.forEach(food => {
+                            foodList.innerHTML = foodList.innerHTML + food + '<br/>';
+                        });
+                        // icon.classList.add("far", "fa-trash-alt");
+
+                        div.appendChild(tableNo);
+                        div.appendChild(foodList);
+                        div.appendChild(totalPrice);
+                        div.appendChild(button);
+
+                        formData.appendChild(div);
+
+                        button.addEventListener('click', () => {
+                            var ref = db.collection("orders").doc(doc.id);
+                            ref.delete();
+                        });
+                    }
+
+                    // Show orders real-time
+                    db.collection("orders").onSnapshot((snap) => {
+                        snap.docChanges().forEach((doc) => {
+                            if (doc.type === "added") {
+                                createFormOrder(doc.doc);
+                            } else if (doc.type === "removed") {
+                                var div = document.querySelector(`[data-id=${doc.doc.id}]`);
+                                try {
+                                    formData.removeChild(div);
+                                } catch {
+                                    window.location.reload();
+                                }
+
+                            }
+                        })
+                    })
+                } else {
+                    document.getElementById("errort").style.display = "";
+                }
+            } else {
+                document.getElementById("errort").style.display = "";
+            }
+        }, error => {
+            document.getElementById("errort").style.display = "";
+        })
+    } catch {
+        document.getElementById("errort").style.display = "";
+    }
 }
